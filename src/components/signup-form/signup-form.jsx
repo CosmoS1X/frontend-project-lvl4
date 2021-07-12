3cdf3503d47f008d0e75409533c230df94be34a7
konstantin.korovin <cosmo.stan@yandex.com>
2021-07-12 17:25:05 +0300

    refactoring

--- src/components/signup-form/signup-form.jsx
+++ src/components/signup-form/signup-form.jsx
@@ -41,7 +41,7 @@ const SignupForm = () => {
         const response = await axios.post(routes.signupPath(), { username, password });
         localStorage.setItem('userId', JSON.stringify(response.data));
         auth.logIn();
-        history.push('/');
+        history.replace('/');
       } catch (err) {
         if (err.isAxiosError && err.response.status === 409) {
           setSignupFailed(true);
