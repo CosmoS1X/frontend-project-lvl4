23a198fe8d462d3e50f2672dbd6aa955fe26b0ab
konstantin.korovin <cosmo.stan@yandex.com>
2021-07-17 16:26:23 +0300

    add translation for not-found page

--- src/pages/not-found-page.jsx
+++ src/pages/not-found-page.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
-const NotFoundPage = () => (
-  <div className="card text-center">
-    <div className="card-body">
-      <h1 className="card-title">Error 404</h1>
-      <h2 className="card-text">Page not found</h2>
+const NotFoundPage = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="container my-4">
+      <div className="card text-center">
+        <div className="card-body">
+          <h1 className="card-title">{t('errors.error404')}</h1>
+          <h2 className="card-text">{t('errors.pageNotFound')}</h2>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default NotFoundPage;
